814b7d6032b5ddb6f6b2c505ba46a8dc90786f43
Muhammad Imran <04muhammadimran@gmail.com>
2020-03-14 23:21:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ bot.on('guildMemberAdd', member => {
     const channel = member.guild.channels.find(channel => channel.name == "welcome");
     if (!channel) return;
     channel.send(`Welcome ${member}, to the Homeless Hallway!`);
-        var defaultrole = member.guild.roles.find("name", "Homeless");
+        var defaultrole = member.guild.roles.find('name', 'Homeless');
         member.addRole(defaultrole)
     
 });
